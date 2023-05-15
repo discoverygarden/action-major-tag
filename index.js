61dfba7a5f50e7979ad00a5bc0adee4efff27d30
Noel Chiasson <noel@discoverygarden.ca>
2023-05-15 13:36:13 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const semver = require('semver');
+
+async function createTag(major_tag) {
+  const token = core.getInput('github_token', { required: false }) || github.context.token;
+  const octokit = github.getOctokit(token);
+  const sha = core.getInput('sha') || github.context.sha;
+  const ref = `refs/tags/${major_tag}`;
+  await octokit.git.createRef({
+    ...github.context.repo,
+    ref,
+    sha
+  });
+
+  console.log(`Tag: "${major_tag}" with sha: "${sha}" successfully written.`);
+}
+
+async function checkIfTagExists(major_tag) {
+  const token = core.getInput('github_token', { required: false }) || github.context.token;
+  const octokit = github.getOctokit(token);
+  const owner = github.context.payload.repository.organization;
+  const repo = github.context.payload.repository.name;
+  const ref = `tags/${major_tag}`;
+
+  try {
+    await octokit.git.getRef({
+      owner,
+      repo,
+      ref,
+    });
+
+    try {
+      console.log(`Ref "${ref}" already exists. Removing to replace.`);
+      await octokit.git.deleteRef({
+        owner,
+        repo,
+        ref,
+      });
+      console.log(`Successfully deleted ref. (ref: "${ref}")`);
+    }
+    catch (error) {
+      console.log(`Failed to delete ref (ref: ${ref}) -  Error: "${error.message}"`);
+    }
+  }
+  catch (error) {
+    console.log(`Ref does not exist. (ref: ${ref}) - Error: "${error.message}"`);
+  }
+}
+
+async function run() {
+  try {
+    const tag = core.getInput('tag') || (github.context.ref).replace('refs/tags/', '');
+    console.log(`Using tag: "${tag}"`);
+    const prefix = core.getInput('prefix') || "";
+    let major_tag = prefix + semver.major(tag);
+    core.setOutput('major_tag', major_tag);
+    core.setOutput('origin_tag', tag);
+    console.log(`Using tag prefix "${prefix}"`);
+    console.log(`Major Release Tag: "${major_tag}" (origin tag: "${tag}")`);
+
+    if (core.getInput('dry_run') !== 'true') {
+      await checkIfTagExists(major_tag);
+      await createTag(major_tag);
+    }
+  }
+  catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
